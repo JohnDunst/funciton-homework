c0baf386149c3c895f0b7608bdcead06faee47ac
JohnDunst <johnnydunst6@outlook.com>
2023-06-06 11:46:48 -0400

    my efforts prior to class

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
-//function maxOfThree(a, b, c) {
+function maxOfThree(a, b, c) 
+{
     if ((a >= b) && (a >= c)) { 
         return a;
     } else if ((b >= a) && (b >= c)) {
@@ -7,14 +8,25 @@
         return c;
     }
 }
-//console.log (maxOfThree (22,90,68));
+console.log (maxOfThree (22,90,68));
 
 
 
 function isCharAVowel
-if (char == ('a'||'e'||'i', 'o'||'u'){
-    console.log("it's a vowel");
-}else{
-    console.log("Its a consanant");
-}
-}
+//if char == ('a'||'e'||'i', 'o'||'u'){//
+  //  console.log("it's a vowel");
+//}else{
+   // console.log("Its a consanant");
+//}
+//}
+
+function sumArray(array) {
+    const ourArray = [1, 4, 0, 9, -3];
+    let sum = 0;
+  
+    for (let i = 0; i < ourArray.length; i += 1) {
+     
+    }
+    
+    return sum;
+  }
